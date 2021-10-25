cbe4d53a08e5d5d886845f58690975384fbad2f4
benjwrdill <benjwrdill@gmail.com>
2021-10-25 15:28:55 -0500

    Getting codewars typescript tests running locally

---
+++ typescript/string_ends_with/test.ts
@@ -0,0 +1,11 @@
+import solution = require('./index');
+// import the type of assertion library you wish to use (Chai recommended)
+import {assert} from "chai";
+
+describe("Sample Test Cases", function(){
+  it("Should return true or false", function() {
+    assert.equal(solution.solution('abcde', 'cde'), true);
+    assert.equal(solution.solution('abcde', 'abc'), false);
+    assert.equal(solution.solution('abc', ''), true);
+  });
+});
